4b2e3f60fe6609cbe6605fd850a305d541614ad3
ThomasLesage86 <lesage.thomass@gmail.com>
2024-11-30 22:25:19 +0100

    2ème save

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-console.log("test from node", Math.random(), new Date())
+const express = require('express');
+const app = express();
+const port = 3000;
+const { firstName, lastName, role} = req.body;
+const lastUserID = users[users.lenght - 1].id;
+const users = [
+    { id: 1, firstName: "John", lastName: "Doe", role: "admin"},
+    { id: 2, firstName: "Jane", lastName: "Smith", role: "user"},
+    { id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator"},
+    { id: 4, firstName: "Bob", lastName: "Brown", role: "user"},
+    { id: 5, firstName: "Charlie", lastName: "Davis", role: "admin"},
+]
+
+// GET : LIRE tous les utilisateurs
+app.get("/", (req, res) => {
+    res.json(users)
+})
+
+app.use(express.json());
+
+// GET : LIRE tous les utilisateurs
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Hello rest api",
+    })
+})
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
+
+// POST : CRÉER un nouvel utilisateur, basé sur les données passées dans le corps(body) de la requête
+app.post("/", (req, res) => {
+	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
+	const { firstName, lastName } = req.body
+
+	// récupérer l'ID du dernier utilisateur en fonction du nombre d'utilisateurs dans notre variable de tableau 'users'.
+	const lastId = users[users.length - 1].id
+	// ajouter un pour créer un utilisateur unique
+	const newId = lastId + 1
+
+	// créer le nouvel utilisateur avec les données du corps de la requête et l'ID calculé
+	const newUser = {
+		firstName,
+		lastName,
+		id: newId,
+	}
+
+	// ajouter le nouvel utilisateur à notre liste d'utilisateurs en utilisant la méthode 'push'
+	users.push(newUser)
+	// envoyer le code de statut 201 (créé) et les données du nouvel utilisateur afin de confirmer au client.
+	res.status(201).json(newUser)
+})
+
+// PUT : MODIFIER un utilisateur basé sur les données envoyées dans le corps(body) et le paramètre passé dans l'uRL
+app.put("/", (req, res) => {
+	res.json({
+		msg: "Hello rest api ici le PUT",
+	})
+})
+
+// DELETE : supprimer un utilisateur basé sur le paramètre passé dans l'URL
+app.delete("/", (req, res) => {
+	res.json({
+		msg: "Hello rest api ici le DELETE",
+	})
+})
+
+
